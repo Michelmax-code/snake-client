bf80911820a9d9eea46cf7da3379a5d902195510
Miguel Jimenez <migang9@gmail.com>
2021-06-02 14:51:33 -0400

    Update name and message to client

--- client.js
+++ client.js
@@ -6,7 +6,7 @@ const connect = function() {
   const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542, }, () => {
-    console.log("Connection established!");
+    console.log("Successfully connected to game server");
     conn.on('data', (data) => {
       console.log('message from server', data);
     });
