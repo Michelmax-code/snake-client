743054eb08b886218b71049cd5d3a3f4025f6732
Miguel Jimenez <migang9@gmail.com>
2021-06-02 14:44:50 -0400

    Update name

--- client.js
+++ client.js
@@ -1,36 +1,27 @@
 const net = require("net");
-const { stdin } = require("process");
+//const { stdin } = require("process");
 
 // establishes a connection with the game server
-const connect = function(data) {
+const connect = function() {
   const conn = net.createConnection({
     host: '135.23.222.131',
     port: 50542, }, () => {
     console.log("Connection established!");
+    conn.on('data', (data) => {
+      console.log('message from server', data);
+    });
   });
-
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
-  stdin.on('connect', (data) => {
-    conn.write('Miguel');
-  })
-  
-  const name = 'Miguel';
-  conn.write(`${name} is back!!`);
-
+  const name = 'MAJ';
+  conn.write(`Name: ${name}`);
+  //stdin.on('connect', (data) => {
+  //  conn.write('Miguel');
+  //});
   //conn.on('data', (whatYouTyped) => {
   //  conn.write(`${name}: ${whatYouTyped}`);
-    
   //})
-  
-  conn.on('data', (data) => {
-    console.log('message from server', data);
-  });
-
   return conn;
-
 };
-
 
 module.exports = connect;
