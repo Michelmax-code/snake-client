edc758b5ff1f8418affca6da47f17504711f6b59
Miguel Jimenez <migang9@gmail.com>
2021-06-02 20:20:39 -0400

    include constants

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
+const {IP, PORT} = require('./constants');
 const net = require("net");
 //const { stdin } = require("process");
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542, }, () => {
+    host: IP,
+    port: PORT, }, () => {
     console.log("Successfully connected to game server");
     conn.write('Name: MAJ');
     conn.on('data', (data) => {
@@ -13,9 +14,22 @@ const connect = function() {
     });
   });
 
-  setInterval(function() {
-    conn.write('Move: up');
-    conn.write('Move: left')}, 500);
+  //  setTimeout(function() {
+  //    conn.write('Move: up')}, 1000);
+  //  setTimeout(function() {
+  //    conn.write('Move: left')}, 2000);
+  //  setTimeout(function() {
+  //    conn.write('Move: down')}, 3000);
+  //  setTimeout(function() {
+  //    conn.write('Move: down')}, 4000);
+  //  setTimeout(function() {
+  //    conn.write('Move: right')}, 5000);
+
+
+  //setInterval(function() {
+  //  conn.write('Move: up');
+  //  conn.write('Move: left');
+  //}, 500);
   // interpret incoming data as text
   conn.setEncoding("utf8");
   const name = 'MAJ';
